3c54198096cc4d633bfa55602fbf64b702b3a4b2
wwj718 <wuwenjie718@gmail.com>
2018-05-28 19:43:35 +0800

    add sensor pipe

--- extension_android.py
+++ extension_android.py
@@ -1,27 +1,53 @@
-import time
+import time, threading
 import zmq
 
 from scratch3_adapter.core_extension import Extension
 from scratch3_adapter import settings
 
 # connect req
+android_ip = "10.10.100.243"
 port = 38778
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
-android_ip = "10.10.100.243"
-socket.connect ("tcp://{}:{}".format(android_ip,port))
+socket.connect("tcp://{}:{}".format(android_ip,port))
 
-class CozmoExtension(Extension):
+# todo 发送传感器数据 thread
+# ip最好都在一处配置，这次让REQ来bind
+port_sensor = 38779
+context_sonsor = zmq.Context()
+socket_sonsor = context_sonsor.socket(zmq.REP)
+socket_sonsor.connect("tcp://{}:{}".format(android_ip,port_sensor))
+
+
+class AndroidExtension(Extension):
     def __init__(self):
         name = type(self).__name__ # class name
         super().__init__(name)
 
-    def run(self):
+    def bg_task(self):
         while True:
+            # recv
+            message = socket_sonsor.recv_json()
+            # print(message)
+            time.sleep(1)
+            self.logger.info("sensor pipe recv: %s", message)
+            socket_sonsor.send_json({"result":"ok"})
+        # 等待android的数据，推到scratch3
+        # 写一个安卓的 scratch3插件
+        # self.publish({})
+
+    def run(self):
+        bg_thread = threading.Thread(target = self.bg_task)
+        self.logger.info("thread start")
+        bg_thread.daemon = True
+        bg_thread.start()
+
+        while True:
+            # send then recv
             message = self.read()
             socket.send_json(message) # 设置超时
             print("send to android server {}".format(message))
             result = socket.recv_json()
             print("result: {}".format(result))
 
-export = CozmoExtension
+export = AndroidExtension
--- src/android_server.py
+++ src/android_server.py
@@ -1,12 +1,18 @@
-import zmq
-import subprocess
+import subprocess, threading, time
 # from zmq.asyncio import Context
+import zmq
 from zmq import Context
 
-port = 38777
+port = 38778
 context = Context.instance()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:%s" % port)
+
+port_sensor = 38779
+context_sonsor = Context.instance()
+socket_sonsor = context_sonsor.socket(zmq.REQ)
+socket_sonsor.bind("tcp://*:%s" % port_sensor)
+
 
 def execute(cmd, encoding='UTF-8', timeout=None, shell=True):
     proc = subprocess.Popen(cmd, stdin=subprocess.DEVNULL,
@@ -17,6 +23,20 @@ def execute(cmd, encoding='UTF-8', timeout=None, shell=True):
     error = error.decode(encoding).rstrip()
     rc = proc.returncode
     return (output, rc, error)
+
+def bg_task():
+    i = 0
+    while True:
+        socket_sonsor.send_json({"hello":f"world{i}"})
+        time.sleep(1)
+        message = socket_sonsor.recv_json()
+        print("sensor pipe recv:",message)
+        print(message)
+        i += 1
+
+bg_thread = threading.Thread(target = bg_task)
+bg_thread.daemon = True
+bg_thread.start()
 
 while True:
         message = socket.recv_json()  # python dict
