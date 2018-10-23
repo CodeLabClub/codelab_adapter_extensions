fd440e43c022317e49937e61b066ce0df06a81cf
wwj718 <wuwenjie718@gmail.com>
2018-10-23 16:04:13 +0800

    Add files via upload

---
+++ extension_blender.py
@@ -0,0 +1,34 @@
+'''
+zmq作为管道 req/rep
+'''
+
+import time
+import zmq
+
+from scratch3_adapter.core_extension import Extension
+from scratch3_adapter import settings
+
+# connect req
+port = 38778
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect ("tcp://localhost:%s" % port)
+
+class BlenderExtension(Extension):
+    '''
+    todo:
+        实现json-rpc
+    '''
+    def __init__(self):
+        name = type(self).__name__ # class name
+        super().__init__(name)
+
+    def run(self):
+        while self._running:
+            message = self.read()
+            socket.send_json(message)
+            print("send to blender server {}".format(message))
+            result = socket.recv_json()
+            print("result: {}".format(result))
+
+export = BlenderExtension
