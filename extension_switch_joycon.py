8115f797a551ea7b78356136c3ca6182776280e7
wwj718 <wuwenjie718@gmail.com>
2018-11-26 15:02:47 +0800

    添加多手柄支持

--- extension_switch_joycon.py
+++ extension_switch_joycon.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python
 # encoding: utf-8
-
 '''
 EIM: Everything Is Message
 '''
-import time
+import sys
+sys.path.append("/usr/local/lib/python3.6/site-packages")
 import pygame
 from pygame.locals import *
 import os
+import time
 
 from scratch3_adapter import settings
 from scratch3_adapter.core_extension import Extension
 
-
 try:
-            os.environ["SDL_VIDEODRIVER"] = "dummy"
-            pygame.init()
-            pygame.joystick.init()
-            joystick0 = pygame.joystick.Joystick(0)
-            # 另一个手柄 pygame.joystick.Joystick(1)
-            joystick0.init()
-            print('joystick start')
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
+    pygame.init()
+    pygame.joystick.init()
+    joystick_count = pygame.joystick.get_count()
+    joysticks = {}
+    for i in range(joystick_count):
+        joysticks[i] = pygame.joystick.Joystick(i)
+        joysticks[i].init()
+        print('joystick {} start'.format(i))
 except pygame.error:
-            print('joystick error')
+    print('joystick error')
+
 
 class JoystickExtension(Extension):
     def __init__(self):
@@ -36,28 +39,27 @@ class JoystickExtension(Extension):
             往scratch不断发送手柄信息
         '''
 
-
         while self._running:
-                # message = {"topic": "eim", "message": "message"}
-                # self.publish(message)
-                # time.sleep(1)
+            # message = {"topic": "eim", "message": "message"}
+            # self.publish(message)
+            # time.sleep(1)
             eventlist = pygame.event.get()
             for e in eventlist:
                 if e.type == QUIT:
                     return
 
                 if e.type == pygame.locals.JOYAXISMOTION:
-                    x, y = joystick0.get_axis(0), joystick0.get_axis(1)
-                    # print 'axis x:' + str(x) + ' axis y:' + str(y)
-                    print('axis x:' + str(x) + ' axis y:' + str(y))
+                    print("axis: {}".format(str(e.dict)))
                 elif e.type == pygame.locals.JOYHATMOTION:
-                    x, y = joystick0.get_hat(0)
-                    # print 'hat x:' + str(x) + ' hat y:' + str(y)
-                    print('hat x:' + str(x) + ' hat y:' + str(y))
+                    # from IPython import embed;embed()
+                    # e中携带了所有信息: e.dict
+                    print("hat: {}".format(str(e.dict)))
+                    # print('hat x:' + str(x) + ' hat y:' + str(y))
                 elif e.type == pygame.locals.JOYBUTTONDOWN:
-                    # print 'button:' + str(e.button)
-                    print('button:' + str(e.button))
-                    # 发往scratch
+                    # 注意 两个手柄是一样的编码
+                    print("button: {}".format(str(e.dict)))
+                    # 只将button信息发往scratch
+                    # todo: eim应该有处理json的能力，对于调试，直接say出来，看是什么即可
                     message = {"topic": "eim", "message": str(str(e.button))}
                     self.publish(message)
 
