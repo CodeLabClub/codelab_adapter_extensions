455b09560c159047e3c13b67e72ef3335afde708
wwj718 <wuwenjie718@gmail.com>
2018-11-27 09:53:59 +0800

    Add files via upload

--- extension_home_assistant.py
+++ extension_home_assistant.py
@@ -1,8 +1,17 @@
 import time
 import subprocess
+import platform
 
 from scratch3_adapter import settings
 from scratch3_adapter.core_extension import Extension
+
+'''
+预先按照好hass，全局的
+'''
+if (platform.system() == "Darwin"):
+    cmd = "/usr/local/bin/hass" # mac
+if platform.system() == "Windows":
+    cmd = ""
 
 class HassExtension(Extension):
     def __init__(self):
@@ -11,7 +20,9 @@ class HassExtension(Extension):
 
     def run(self):
         # run 会被作为线程调用,勾选之后调用: 启动hass进程
-        hass = subprocess.Popen("hass", shell = True)
+        # 用户需要知道系统命令所在的绝对路径
+        
+        hass = subprocess.Popen(cmd , shell = True)
         while self._running:
             # 把while当成阻塞机制
             time.sleep(1)
