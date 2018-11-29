4805af3aaab445f9cb24ff492d8b98172b4380a6
wwj718 <wuwenjie718@gmail.com>
2018-11-29 14:08:09 +0800

    Add files via upload

---
+++ extension_test2.py
@@ -0,0 +1,30 @@
+import time
+
+from scratch3_adapter import settings
+from scratch3_adapter.core_extension import Extension
+from guizero import info
+
+
+def doRGBLed(ser, index = 0, red = 0, green = 0, blue = 0):
+	ser.write(bytearray([0xff,0x55,0x9,0x0,0x2,0x8,0x7,0x2,index,red,green,blue]))  
+
+class Test2Extension(Extension):
+    def __init__(self):
+        name = type(self).__name__ # class name
+        super().__init__(name)
+        # self.ser = serial.Serial("COM3", 115200, timeout=1)
+    def run(self):
+        # run 会被作为线程调用
+        while self._running:
+            message = self.read()
+            self.logger.debug("message:%s",str(message))
+            data = message.get("data")
+            if data:
+                R = int(data.get("R"))
+                G = int(data.get("G"))
+                B = int(data.get("B"))
+                self.logger.info("RGB: {} {} {}".format(R,G,B))
+                info("info","RGB: {} {} {}".format(R,G,B))
+                # doRGBLed(red=R, green=G, blue=B)
+
+export = Test2Extension
