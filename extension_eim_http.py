4c70cb35f231496b079c53a372906a9ec4bbefb7
wwj718 <wuwenjie718@gmail.com>
2018-12-06 09:50:36 +0800

    Add files via upload

---
+++ extension_eim_http.py
@@ -0,0 +1,35 @@
+import time
+import requests
+from scratch3_adapter import settings
+from scratch3_adapter.core_extension import Extension
+from scratch3_adapter.utils import  ui_error
+
+class EimHttpExtension(Extension):
+    def __init__(self):
+        '''
+        异步的http
+        '''
+        name = type(self).__name__  # class name
+        super().__init__(name)
+
+    def run(self):
+        while self._running:
+            message = self.read()
+            data = message.get('data')
+            topic = message.get("topic")
+            if (topic == "eim") and data:
+                # 返回json还是text
+                # url = "http://github.com"
+                try:
+                    url = data
+                    r = requests.get(url)
+                    response = r.text
+                except:
+                    response = "something error"
+                message = {"topic": "eim", "message": response}
+                self.publish(message)
+                    
+                    
+
+
+export = EimHttpExtension
