90195185ab575ccc065a9c51f144ae1fc8332876
wwj718 <wuwenjie718@gmail.com>
2019-01-11 14:52:25 +0800

    Add files via upload

---
+++ extension_presentation.py
@@ -0,0 +1,40 @@
+from io import StringIO
+import contextlib
+import sys
+import subprocess
+import webbrowser
+from scratch3_adapter import settings
+from scratch3_adapter.core_extension import Extension
+
+class PresentationExtension(Extension):
+    def __init__(self):
+        name = type(self).__name__  # class name
+        super().__init__(name)
+
+    @contextlib.contextmanager
+    def stdoutIO(self, stdout=None):
+        old = sys.stdout
+        if stdout is None:
+            stdout = StringIO()
+        sys.stdout = stdout
+        yield stdout
+        sys.stdout = old
+
+    def run(self):
+        while self._running:
+            if settings.DEBUG:
+                message  = self.read()
+                self.logger.info(message)
+                python_code = message.get('data')
+                try:
+                    with self.stdoutIO() as s:
+                        # 不用rpc, 使用dynamicland的架构原则: 保持单一入口, message is everything (everything is message)
+                        exec(python_code)
+                    output = s.getvalue()
+                except Exception as e:
+                    output = str(e)
+                message = {"topic": "eim", "message": str(output)}
+                self.publish(message)
+
+
+export = PresentationExtension
