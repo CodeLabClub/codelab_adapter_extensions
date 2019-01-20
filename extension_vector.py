676130bf584f30461f31853437f684c6c9c78050
wwj718 <wuwenjie718@gmail.com>
2019-01-20 17:16:47 +0800

    update extension vector

--- extension_vector.py
+++ extension_vector.py
@@ -1,111 +1,65 @@
-'''
-Vector: 
-    https://developer.anki.com/vector/docs/getstarted.html
-    https://github.com/anki/vector-python-sdk/blob/master/examples/tutorials/02_drive_square.py
-todo:
-  整合到adapter中 整个打包
-'''
-import time
-import threading
-# https://github.com/pavlov99/json-rpc
-# from jsonrpc import JSONRPCResponseManager, dispatcher
-#  参考: https://github.com/Scratch3Lab/scratch3_adapter_extensions/blob/master/src/cozmo_server.py
-from scratch3_adapter import settings
+import zmq
+import subprocess
+import pathlib
+import platform
+
 from scratch3_adapter.core_extension import Extension
-
-import sys
-# 都使用 pip3 install opencv-python --user
-sys.path.append("/Users/wuwenjie/Library/Python/3.6/lib/python/site-packages")
-import anki_vector
-from anki_vector.util import degrees, distance_mm, speed_mmps
+from scratch3_adapter import settings
 
 
-# 使用一个代理 proxy
-class VectorProxy:
-    def __init__(self, data, robot):
-        self.method = data.get("method")
-        self.params = data.get("params")
-        self.robot = robot
-        self.result = {}
+def get_python3_path():
+    # If it is not working,  Please replace python3_path with your local python3 path. shell: which python3
+    if (platform.system() == "Darwin"):
+        # which python3
+        path = "/usr/local/bin/python3"
+    if platform.system() == "Windows":
+        path = "python"
+    if platform.system() == "Linux":
+        path = "/usr/bin/python3"
+    return path
 
-    def run(self):
-        '''    
-        payload = {
-            "method": "echo",
-            "params": {"a":1,"b":2},
-            # "jsonrpc": "2.0",
-            # "id": 0,
-        }
-        '''
-        robot_match = {
-            # "vector/sensor": self.handle_sensor,
-            "vector/say": self.say_text,
-            "vector/play": self.play_anim,
-            "vector/drive_straight": self.drive_straight,
-            "vector/turn": self.turn,
-        }
-        # go on
-        handle = robot_match.get(self.method, None)
-        if handle:
-            handle()
-            self.result["state"] = "success"
-        else:
-            self.result["state"] = self.no_match_handle()
-        return self.result
 
-    def say_text(self):
-        # ? 按位置会不会更简单
-        self.robot.say_text(self.params.get("content"))
-
-    def play_anim(self):
-        # data : anim_name
-        self.robot.play_anim(name=self.params.get("name"))
-
-    def drive_straight(self):
-        # Drive forwards for 150 millimeters at 50 millimeters-per-second.
-        distance = int(self.params.get("distance_mm", 50))
-        speed = int(self.params.get("speed_mmps", 25))
-        self.robot.behavior.drive_straight(
-            distance_mm(distance), speed_mmps(speed))
-
-    def turn(self):
-        # Turn 90 degrees to the left.
-        # Note: To turn to the right, just use a negative number.
-        angle = int(self.params.get("angle", 90))
-        speed = int(self.params.get("speed", 45))
-        self.robot.behavior.turn_in_place(degrees(angle), speed=degrees(speed))
-
-    def handle_sensor(self):
-        result = {}
-        result["accelerometer"] = [
-            self.robot.accelerometer.x, self.robot.accelerometer.y,
-            self.robot.accelerometer.z
-        ]
-        return result
-
-    def no_match_handle(self):
-        # 404
-        return "404 on match"
+python3_path = get_python3_path()
 
 
 class VectorExtension(Extension):
     def __init__(self):
-        name = type(self).__name__  # class name
+        name = type(self).__name__
         super().__init__(name)
+        self.TOPIC = "eim/vector"  # eim + [extension id]
 
     def run(self):
-        # 先启动vector
 
-        with anki_vector.Robot() as robot:
-            while self._running:
-                message = self.read()  # json
-                self.logger.info("message:%s", str(message))
-                topic = message.get("topic")
-                data = message.get("data")
-                if data.get("method"):
-                    vector_proxy = VectorProxy(data, robot)
-                    result = vector_proxy.run()
-                    self.logger.info(result)
+        port = 38781
+        context = zmq.Context.instance()
+        socket = context.socket(zmq.REQ)
+        socket.connect("tcp://localhost:%s" % port)
+        scratch3_adapter_server_dir = pathlib.Path.home(
+        ) / "scratch3_adapter" / "servers"
+        script = "{}/vector_server.py".format(scratch3_adapter_server_dir)
+
+        cmd = [python3_path, script]
+        vector_server = subprocess.Popen(cmd)
+        settings.running_child_procs.append(vector_server)
+
+        while self._running:
+            message = self.read()  # 有可能有阻塞在这里，关掉插件后，还需要scratch3发一条消息。
+            self.logger.debug(message)
+            topic = message.get('topic')
+            python_code = message.get('data')
+            if topic == self.TOPIC:
+                socket.send_json({"python_code": python_code})
+                result = socket.recv_json().get("result")
+                # 发往scratch3.0
+                self.publish({"topic": self.TOPIC, "message": result})
+
+        # release socket
+        socket.send_json({"python_code": "quit!"})
+        result = socket.recv_json().get("result")
+        vector_server.terminate()
+        vector_server.wait()
+        socket.close()
+        context.term()
 
 
 export = VectorExtension
---
+++ servers/vector_server.py
@@ -0,0 +1,40 @@
+'''
+pip3 install anki_vector --user
+python3 vector.py # should be ok
+'''
+import anki_vector
+import zmq
+
+# zmq socket
+port = 38781
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:%s" % port)
+
+quit_code = "quit!"
+
+with anki_vector.Robot() as robot:
+    while True:
+        python_code = socket.recv_json().get("python_code")
+        if not python_code:
+            continue
+
+        if python_code == "quit!":
+            socket.send_json({"result": "quit!"})
+            break
+        else:
+            try:
+                # 单一入口，传递源码
+                # 为了安全性, 做一些能力的牺牲, 放弃使用exec
+                output = eval(python_code, {"__builtins__": None}, {
+                    "anki_vector": anki_vector,
+                    "robot": robot
+                })
+                # exec(python_code) # 安全性问题
+            except Exception as e:
+                output = e
+        socket.send_json({"result": str(output)})
+
+    socket.close()
+    context.term()
+    robot.disconnect()
