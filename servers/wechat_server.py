4e400010472d78aa3c6cf20c2aecbfcc108a91c6
wwj718 <wuwenjie718@gmail.com>
2019-02-09 14:58:53 +0800

    Add files via upload

--- servers/wechat_server.py
+++ servers/wechat_server.py
@@ -1,27 +1,37 @@
 '''
-pip3 install itchat
+pip3 install itchat --user
 
-构建管道
+构建2个管道
 '''
 import itchat, time
 from itchat.content import TEXT
 import zmq
 import threading
+quit_code = "quit!"
+
+port = 38783  # req 发送微信消息
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:%s" % port)
 
 
 # 线程
 def message_monitor():
-    # recv scratch
-    # quit_code = "quit!"
-    rep_port = 38784
+    # recv from scratch
+    rep_port = 38784  # 从这里退出
     rep_context = zmq.Context()
     rep_socket = rep_context.socket(zmq.REP)
     rep_socket.bind("tcp://*:%s" % rep_port)
-    # get message from scratch
     while True:
         message = rep_socket.recv_json()
+        # 退出 socket
         username = message["username"]
         text = message["text"]
+        if text == quit_code:
+            socket.send_json({"text": quit_code,"username":""}) # 38783
+            rep_socket.send_json({"result": "get it!"})
+            break
+
         rep_socket.send_json({"result": "get it!"})
         try:
             user2SendMessage = itchat.search_friends(nickName=username)[0]
@@ -34,11 +44,6 @@ bg_task = threading.Thread(target=message_monitor)
 bg_task.daemon = True
 bg_task.start()
 
-port = 38783  # req 发送微信消息
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:%s" % port)
-
 
 @itchat.msg_register([TEXT])
 def text_reply(msg):
@@ -49,7 +54,7 @@ def text_reply(msg):
     user = msg.user["NickName"]
     if content == "codelab":
         user.send("hi 感谢参与测试：）")
-    socket.send_json({"username":user,"text":content})
+    socket.send_json({"username": user, "text": content})
     socket.recv_json().get("result")
 
 
