a9415e570bba3e8ad65cba19332d0e53b521a544
wangshub <rocksong.hit@gmail.com>
2019-04-15 16:41:55 +0800

    feat(extension): new pando extension file from EIM hello_world template

---
+++ extension_leju_pando.py
@@ -0,0 +1,18 @@
+import time, threading, subprocess
+from codelab_adapter import settings
+from codelab_adapter.core_extension import Extension
+
+class HelloworldExtension(Extension):
+    def __init__(self):
+        name = type(self).__name__
+        super().__init__(name)
+
+    def run(self):
+        while True:
+            message = self.read()
+            if message["topic"] == "eim":
+                subprocess.call("say {}, my name is pando from leju robotics".format(message["payload"]), shell=True)
+
+
+export = HelloworldExtension
+
