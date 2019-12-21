da2bbb98364c6e5e8bcc0b3d014b45a7539106cd
wwj718 <wuwenjie718@gmail.com>
2019-12-21 13:36:55 +0800

    Update extension_kano_wand.py

--- extensions_v2/extension_kano_wand.py
+++ extensions_v2/extension_kano_wand.py
@@ -4,6 +4,7 @@ todo 使用bottle
 '''
 
 import time
+import socket
 from codelab_adapter.core_extension import Extension
 from codelab_adapter.utils import threaded
 import webbrowser
@@ -17,7 +18,10 @@ def index(name):
     html = '''
     ok!
     '''
-    # if name == "wand":
+    if name == "hello":
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        html = "\nok!<p>IP:" + s.getsockname()[0] + "</p>\n"
     q.put(name)
     return template(html, name=name)
 # http://127.0.0.1:8080/wand/motion1
@@ -34,6 +38,7 @@ class WebServerExtension(Extension):
 
     def run(self):
         self.run_webserver_as_thread()
+        webbrowser.open(f'http://127.0.0.1:{self.port}/wand/hello')
         time.sleep(0.5)
         while self._running:
             message_web = q.get()
