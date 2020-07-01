c743a9b740ac1ab4154163dce7632f7882330435
wwj718 <wuwenjie718@gmail.com>
2020-07-01 11:37:48 +0800

    add

--- extensions_v3/extension_Siri.py
+++ extensions_v3/extension_Siri.py
@@ -2,13 +2,13 @@
 ref: https://bottlepy.org/docs/dev/tutorial.html
 
 fork from https://github.com/CodeLabClub/codelab_adapter_extensions/blob/master/extensions_v3/extension_webserver.py
-
 '''
 
 import time
 import webbrowser
-import requests
+import queue
 
+import requests
 import bottle
 from bottle import route, run, template, view, request
 
@@ -16,7 +16,8 @@ from codelab_adapter.core_extension import Extension
 from codelab_adapter.utils import threaded, get_local_ip
 from codelab_adapter_client.utils import get_adapter_home_path
 # html 文件所在目录
-PORT = 18082
+PORT = 18081
+message_queue = queue.Queue()
 
 bottle.TEMPLATE_PATH = [
     get_adapter_home_path() / "extensions",  # 优先搜索 html 模版所在目录
@@ -29,12 +30,17 @@ def index(name="world"):
     ip = get_local_ip()
     html = '''
     <p>IP: {{ip}}</p>
-    <p>EIM point: http://{{ip}}:{{PORT}}/api/message/eim?message=1</p>
+    <p>在 iOS 快捷指令里创建以下捷径(
+    )</p>
+    <img width=400 src="https://adapter.codelab.club/img/IMG_0015.PNG"/>
+    </br>
+    <a target="_blank" href="https://adapter.codelab.club/extension_guide/siri/">文档</a>
+    <!--<p>Siri point: http://{{ip}}:{{PORT}}/api/message/siri?message=1</p>-->
     '''
     return template(html, name=name, ip=ip, PORT=PORT)
 
 
-@route('/api/message/eim', method='POST')
+@route('/api/message/siri', method='POST')
 def eim_post():
     # http post http://192.168.31.148:18080/eim/post message=1, iphone 捷径
     # 中文 使用post
@@ -42,22 +48,18 @@ def eim_post():
     message = json_dict.get("message", None)
     if message:
         # print(message) # eim
-        requests.get(
-            f"https://codelab-adapter.codelab.club:12358/api/message/eim?message={message}"
-        )
-        print(f"message, {message}")
+        # print(f"message, {message}")
+        message_queue.put(message)
         return "ok"
 
 
 # one = request.GET.get('one', '').strip()
-@route('/api/message/eim', method='GET')
+@route('/api/message/siri', method='GET')
 def eim_get():
     message = request.GET.get('message', '').strip()
     if message:
-        requests.get(
-            f"https://codelab-adapter.codelab.club:12358/api/message/eim?message={message}"
-        )
-        print(f"message, {message}")
+        # 发送到独立频道
+        message_queue.put(message)
         return "ok"
 
 
@@ -72,9 +74,9 @@ def hello_html(name="world"):
 
 
 class WebServerExtension(Extension):
-    NODE_ID = "eim/extension_webserver"
-    HELP_URL = "https://adapter.codelab.club/extension_guide/webserver/"
-    DESCRIPTION = "http eim adapter"
+    NODE_ID = "eim/extension_Siri"
+    HELP_URL = "https://adapter.codelab.club/extension_guide/siri/"
+    DESCRIPTION = "Hey Siri"
 
     def __init__(self):
         super().__init__()
@@ -95,7 +97,13 @@ class WebServerExtension(Extension):
             ip = "127.0.0.1"
         webbrowser.open(f'http://{ip}:{self.port}/hi/codelab')  # 局域网
         while self._running:
-            time.sleep(0.5)
+            if not message_queue.empty():
+                http_message = message_queue.get()
+
+                message = self.message_template()
+                message["payload"]["content"] = http_message
+                self.publish(message)
+            time.sleep(1 / 10)
 
 
 export = WebServerExtension
