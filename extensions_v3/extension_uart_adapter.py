b12021426f4defcae8150b393b2e1f739a6a3d3d
wwj718 <wuwenjie718@gmail.com>
2020-07-13 14:11:48 +0800

    update

--- extensions_v3/extension_uart_adapter.py
+++ extensions_v3/extension_uart_adapter.py
@@ -1,63 +1,6 @@
-'''
-python -m serial.tools.miniterm <port_name> -h
-python -m serial.tools.list_ports
-
-todo
-    parity 启用奇偶校验
-'''
-
 import time
-import serial
-from serial.tools.list_ports import comports as list_serial_ports
+from codelab_adapter.uart_adapter import serialHelper
 from codelab_adapter.core_extension import Extension
-
-
-class serialHelper:
-    # 每当连接新的port 就把既有的断掉
-    def __init__(self, extensionInstance):
-        self.extensionInstance = extensionInstance
-        self.logger = extensionInstance.logger
-        self.port = None  # 当前连接的port
-        self.ser = None
-
-    def connect(self, port, **kwargs):
-        self.logger.debug(f"args: {kwargs}")
-        if self.ser:
-            self.ser.close()
-        # 默认支持microbit, timeout 默认不超过scratch的积木 5s (4.5)
-        if not kwargs.get("baudrate", None):
-            kwargs["baudrate"] = 115200
-        if not kwargs.get("timeout", None):
-            kwargs["timeout"] = 4.5
-
-        self.ser = serial.Serial(port, **kwargs)
-        if self.ser.name:
-            return "ok"
-
-    def list_ports(self):
-        return [i[0] for i in list_serial_ports()]
-
-    def update_ports(self):
-        ports = [i[0] for i in list_serial_ports()]
-        message = self.extensionInstance.message_template()
-        message["payload"]["content"] = {
-            "ports": ports,
-        }
-        self.extensionInstance.publish(message)
-        return "ok"
-
-    def write(self, content):
-        if self.ser:
-            self.ser.write(content.encode('utf-8'))
-            return "ok"
-
-    def readline(self):
-        if self.ser:
-            content = self.ser.readline()  # timeout
-            if content:
-                content_str = content.decode()
-                return content_str.strip()
-
 
 class SerialExtension(Extension):
 
