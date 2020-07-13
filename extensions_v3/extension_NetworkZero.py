5e38fb7cd610c1637e17288a13b3f8c60c3f7ab0
wwj718 <wuwenjie718@gmail.com>
2020-07-13 14:14:39 +0800

    update

--- extensions_v3/extension_NetworkZero.py
+++ extensions_v3/extension_NetworkZero.py
@@ -1,59 +1,10 @@
 import queue
 import time
 
-from codelab_adapter_client.utils import threaded
 from codelab_adapter.core_extension import Extension
-
-import networkzero as nw0
+from codelab_adapter.nw0_adapter import Nw0Helper
 
 nw0_message_queue = queue.Queue()
-
-
-class Nw0Helper:
-    def __init__(self, extensionInstance):
-        self.extensionInstance = extensionInstance
-        self.logger = extensionInstance.logger
-        self.addresses = None  # 当前连接的port
-
-    @threaded
-    def _wait_for_message(self, name, address):
-        while True:  # thread
-            message = nw0.wait_for_message_from(address)
-            nw0.send_reply_to(address, "ok")
-            # todo send to scratch
-            print(f"address -> {address}; message -> {message}")  # queue
-            nw0_message_queue.put((name, message))
-
-    def advertise(self, content):
-        # 如果存在则不能再广播
-        address = nw0.advertise(content, fail_if_exists=True)  # 强行 只允许一次
-        if address:
-            self._wait_for_message(content, address)
-            return address
-        else:
-            # 已经存在
-            return "already exists!"
-
-    def discover(self, content):
-        address = nw0.discover(content, wait_for_s=1)
-        return address
-
-    def discover_all(self):
-        addresses = nw0.discover_all()
-        return addresses
-
-    def update_addresses(self):
-        addresses = nw0.discover_all()
-        message = self.extensionInstance.message_template()
-        message["payload"]["content"] = {
-            "addresses": addresses, # [['a','xxx'],]
-        }
-        self.extensionInstance.publish(message)
-        return "ok"
-
-    def send_message_to(self, address, content):
-        reply = nw0.send_message_to(address, content)
-        return reply
 
 
 class NW0Extension(Extension):
@@ -67,7 +18,7 @@ class NW0Extension(Extension):
 
     def __init__(self):
         super().__init__()
-        self.nw0Helper = Nw0Helper(self)
+        self.nw0Helper = Nw0Helper(self, nw0_message_queue)
 
     def run_python_code(self, code):
         # fork from python extension
@@ -93,9 +44,10 @@ class NW0Extension(Extension):
         while self._running:
             time.sleep(0.05)
             if not nw0_message_queue.empty():
-                
+
                 (name, nw0_message) = nw0_message_queue.get()
-                self.logger.debug(f'(name, nw0_message)(to scrtch) -> {(name, nw0_message)}')
+                self.logger.debug(
+                    f'(name, nw0_message)(to scrtch) -> {(name, nw0_message)}')
                 message = self.message_template()
                 # pub to scratch
                 message["payload"]["content"] = {
